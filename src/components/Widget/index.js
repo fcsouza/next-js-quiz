dd58a9c319b07d415644ce26687fa705f32096e4
fcsouza <fcsouza18@hotmail.com>
2021-01-27 20:29:52 -0300

    include styles

---
+++ src/components/Widget/index.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  border-radius: 4px;
+  overflow: hidden;
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+  input{
+    width:100%;
+    margin: 30px 0;
+    display: block;
+  }
+  button {
+    text-align: center;
+    margin: 5px 0;
+    border: 1px solid black; 
+    padding: 14px 40px;
+    border-radius: 3px;
+    display: block;
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.secondary};
+    color: ${({ theme }) => theme.colors.mainBg};
+  }
+`;
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color:#FFF;
+  
+  * {
+    margin: 0;
+  }
+`;
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+
+
+export default Widget;
