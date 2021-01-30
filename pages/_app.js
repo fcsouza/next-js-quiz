ad46387e26dc24f2e30acc045228c8e8d06791b5
fcsouza <fcsouza18@hotmail.com>
2021-01-29 21:43:59 -0300

    update db

--- pages/_app.js
+++ pages/_app.js
@@ -33,13 +33,12 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
         <meta property="og:url" content={db.url} />
         <meta property="og:title" content={db.title} />
         <meta property="og:description" content={db.description} />
-        <meta property="og:image" content={db.bg} />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
-        
+        <meta property="og:image" content={db.bg} />      
       </Head>
 
       <ThemeProvider theme={theme}>
