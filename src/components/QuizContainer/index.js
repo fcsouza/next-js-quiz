0d96014e828ac6c87a475482386bb532cee64333
fcsouza <fcsouza18@hotmail.com>
2021-02-13 17:34:58 -0300

    include api fetch

--- src/components/QuizContainer/index.js
+++ src/components/QuizContainer/index.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 
 const QuizContainer = styled.div`
   width: 100%;
-  max-width: 450px;
+  max-width: 350px;
   padding-top: 45px;
-  margin: auto 10%;
+  margin: auto 13%;
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
