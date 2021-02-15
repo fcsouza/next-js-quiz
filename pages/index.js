cf499d080571f7ead86c4f6eae61485116ef4cd2
fcsouza <fcsouza18@hotmail.com>
2021-02-15 17:44:02 -0300

    update index

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,136 @@
 import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
+import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
-import QuizLogo from '../src/components/QuizLogo'
+import Link from '../src/components/Link';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
-import QuizContainer from '../src/components/QuizContainer';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
 
+import { Lottie } from '@crello/react-lottie';
+
+import loadingAnimation from '../src/screens/Quiz/animations/890-loading-animation.json';
+
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  //hook
   const router = useRouter();
-  const [name,setName] = React.useState('');
+  const [name, setName] = React.useState('');
 
-  return (        
+  return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>
+          AluraQuiz -
+          {db.title}
+        </title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
+
+          <Lottie
+            width="200px"
+            height="200px"
+            className="lottie-container basic"
+            config={{ animationData: loadingAnimation, loop: true, autoplay: true }}
+          />
+
+
             <p>{db.description}</p>
-            <form onSubmit={function(event){
-              event.preventDefault();
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
-            }}>
-              <Input 
+            }}
+            >
+              <Input
                 name="nomeDoUsuario"
-                placeholder="Seu nome " 
-                onChange={(evento)=> { setName(evento.target.value) }}
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="to start enter your name..."
                 value={name}
-                />
-              <Button type="submit" disabled={name.length===0 }> 
-               { `Jogar ${name}`}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
               </Button>
             </form>
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
-            <h1>Quizes da Galera</h1>
+            <h1>Try others...</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
-        <Footer />
+        <Footer
+          as={motion.footer}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/fcsouza" />
     </QuizBackground>
